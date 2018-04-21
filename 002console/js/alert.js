77be8b862c73cf349cb481e2ad2ebaf669e292d6
WFlorisGU <544642612@qq.com>
2018-04-21 13:50:53 +0800

    modify

---
+++ 002console/js/alert.js
@@ -0,0 +1,12 @@
+
+// alert("hhhhh跟老师一起疯~");      ctrl加斜杆，选中范围全部注释掉；重复操作回复原样
+// alert("啦啦啦啦啦啦啦~");           同样可以用在html的注释里面
+function taoLu(){
+    alert("全场燥起来");
+    alert("扭动你的肢体哈哈哈哈");
+    alert("一起摇摆~~~~");
+}
+
+taoLu();
+taoLu();
+taoLu();
